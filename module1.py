abb2af4b592e8694bd7a3cd7dd7571461e51641d
kirfill <mr.fks05@mail.ru>
2021-02-01 18:01:44 +0600

    fff

--- module1.py
+++ module1.py
@@ -1,9 +1,22 @@
-sum = 0
-with open("Perepis.txt", "r") as f:
-     for i in f:
-         print(i)
-         if int(i[-5:])<1978:
-             sum+=1
-print(sum)
+u1 = int(input("u1"))
+u2 = int(input("u2"))
+t = int(input("t"))
+
+
+
+def dekf(f):
+     def fg():
+         s = t*u1
+         print(s)
+         f()
+     return fg
+
+@dekf
+def f():
+     a = u2-u1/t
+     print(a, "ускорение")
+
+f()
+
 
 
